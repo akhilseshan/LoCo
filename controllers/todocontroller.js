08b8a22c33567b66d01a695ee326efd6f84bfd94
MeenakshiShankar <me30shankar@gmail.com>
2019-10-02 17:51:08 +0530

    ini3

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,6 +1,10 @@
+var data=[{item:'get milk'},{item:'walk dog'},{item:'assgn2'}]
+
+
+
 module.exports=function(app){
 app.get('/todo',function(req,res){
-   res.render('todo');
+   res.render('todo',{todos:data});
 });
 
 app.post('/todo',function(req,res){});
