42432223f8a8cd7ec12aba285ae54c46d296c842
MeenakshiShankar <me30shankar@gmail.com>
2019-10-02 10:12:19 +0530

    initial

---
+++ public/app.js
@@ -0,0 +1,14 @@
+var express=require('express');
+
+var todocontroller=require('./controllers/todocontroller');
+var app=express();
+
+app.set('view engine',"ejs");
+
+app.use(express.static('./public'))
+
+todocontroller(app);
+
+app.listen(3000);
+
+console.log('you are listening to port 3000');
