c13cc06bf5e55a43db4f06885490e244d6327b49
MeenakshiShankar <me30shankar@gmail.com>
2019-10-21 18:26:51 +0530

    in9

--- app.js
+++ app.js
@@ -19,11 +19,12 @@ mongoose.connect("mongodb://usermee_30:aim2reach@cluster0-shard-00-00-yofix.mong
 
 
     app.post('/efgh', (req,res) => { 
-    console.log(req.body);
-    var localuser1 = req.user.pickup;
-    var localuser2 = req.user.destination;
-
     
+    console.log(req.body);
+    var localuser1 = req.body.pickup;
+    var localuser2 = req.body.destination;
+     
+        
     console.log(localuser1);
     console.log(localuser2);
 
@@ -42,8 +43,11 @@ mongoose.connect("mongodb://usermee_30:aim2reach@cluster0-shard-00-00-yofix.mong
 });
 
 app.get('/efgh',(req,res) => {
-    request("https://www.google.com/maps/embed/v1/directions?key=AIzaSyB_dvpiDyg07irvwzeHg2afFILSaXRXH7E&origin=" +{localuser1:req.user.pickup}+ "&destination=" +{localuser2:req.user.destination}+"&avoid=tolls|highways", function (error, response, body){
+     
+    
+    request("https://www.google.com/maps/embed/v1/directions?key=AIzaSyB_dvpiDyg07irvwzeHg2afFILSaXRXH7E&origin=" +{localuser1:req.get('pickup')}+ "&destination=" +{localuser2:req.get('destination')}+"&avoid=tolls|highways", function (error, response, body){
     //request("https://www.google.com/maps/embed/v1/directions?key=AIzaSyB_dvpiDyg07irvwzeHg2afFILSaXRXH7E&origin=" +localuser1+ "&destination=" +localuser2+"&avoid=tolls|highways", function (error, response, body) {
+    console.log(req.body);   
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
